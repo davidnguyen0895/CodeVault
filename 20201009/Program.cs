21a6824d25c749171e380e215eeec7073f7df37d
brocodeLeo <thinh.nguyen0895@yahoo.com>
2020-10-10 00:53:22 +0900

    test 20201010

---
+++ 20201009/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace _20201009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte score = 75;
+            Console.WriteLine(score >= 70 ? "合格" : "不合格");
+            Console.ReadLine();
+        }
+    }
+}
